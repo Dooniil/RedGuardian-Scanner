c081dd6bd0cbaad74c7a1962b65a2645079ce441
Daniil <dooniil2@yandex.ru>
2023-01-22 01:14:24 +0300

    wmi winrm ssh

--- Scanners.py
+++ Scanners.py
@@ -1,5 +1,7 @@
 import nmap3
 import wmi
+import winrm
+import paramiko
 from enum import Enum
 
 from datetime import datetime
@@ -38,22 +40,48 @@ class HostDiscoveryScanner(IScanner):
 
 class Transport(Enum):
     WMI = 0,
-    WinRM = 1
+    WinRM = 1,
+    SSH = 2
+
+
+# class TransportLinux(Enum):
+#     SSH = 0
+
 
 #  TODO надо разбить на классы будет, или как-то, способы проверок в зависимости от уязвимости, ибо тут
 #   мы используем один Win32, а в другой уязвимости будет другая API
 class VulnerabilitiesScanner(IScanner):
 
-# Для обновлений
+    #   Для обновлений
     def scan(self, body: dict) -> any:
-        transport: Transport = body['transport_type']
-        if transport in Transport.WMI.value:
-            try:
-                connection = wmi.WMI(body['host'], user=body['user_login'], password=body['pwd_login'])
+        transport: int = body['transport_type']
+        try:
+            if transport == Transport.WMI.value:
+                connection = wmi.WMI(computer=body['host'], user=body['user_login'], password=body['pwd_login'])
                 query_result = connection.Win32_QuickFixEngineering()
                 return {zip(
-                        [i for i in range(0, len(query_result))],
-                        [j.HotFixID for j in query_result]
-                    )}
-            except Exception as e:
-                print(e)
+                    [i for i in range(0, len(query_result))],
+                    [j.HotFixID for j in query_result])}
+            elif transport == Transport.WinRM.value:
+                connection = winrm.Session(
+                    f'http://{body["host"]}:5985/wsman',
+                    auth=(body['user_login'], body['pwd_login']),
+                    server_cert_validation='ignore',
+                    transport='ntlm',
+                    message_encryption='auto')
+                query_result = connection.run_ps('Get-HotFix').std_out.decode('utf-8')
+                print(query_result)
+            elif transport == Transport.SSH.value:
+                connection = paramiko.SSHClient()
+                connection.load_system_host_keys()
+                connection.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+                connection.connect(
+                    hostname=body['host'],
+                    username=body['user_login'],
+                    password=body['pwd_login'],
+                    port=22)
+                _, stdout, _ = connection.exec_command('ip address')
+                print(stdout.read())
+                connection.close()
+        except Exception as e:
+            print(e)
--- main.py
+++ main.py
@@ -4,5 +4,11 @@ from ScannerController import ScannerController
 if __name__ == "__main__":
     # elevate()
     controller = ScannerController()
-    body = {'host': '10.0.0.147', 'scan_type': 1, 'transport_type': 0, 'user_login': '', 'pwd_login': 'P@ssword12-'}
+    body = {
+        'host': '172.24.28.215',
+        'scan_type': 1,
+        'transport_type': 2,
+        'user_login': 'scanner_user',
+        'pwd_login': 'P@ssword12-'
+    }
     controller.start_scan(body)
